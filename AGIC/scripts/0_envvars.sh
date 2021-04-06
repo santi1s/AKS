28460a44e9631c373d7b1bf6345067ed32588c41
santi1s <sesantia@microsoft.com>
2021-04-06 14:59:52 +0100

    COmmants changed

--- AGIC/scripts/0_envvars.sh
+++ AGIC/scripts/0_envvars.sh
@@ -10,3 +10,5 @@ export NODE_SUBNET_NAME=aks-agicingress-nodesubnet
 export APPGW_PIP_NAME=aksagicingress-AppGw-PublicIP
 export APPGW_SUBNET_NAME=aksagicingress-AppGw-subnet
 export APPGW_NAME=aksagicingress-AppGw
+#User Identity
+export AGIC_IDENTITY=aksagicingress-identity
