bee8e076dfd712eedd73ebaff5edea1e2a652112
santi1s <sesantia@microsoft.com>
2021-04-16 21:23:35 +0100

    deleted nginx

--- NGINX-ING-CONTROLLER/Scenario-3-NGINX/check_certs.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo -e "\nSHA1 Fingerprint from cert used to create TLS CM"
-openssl x509 -in certs/server.crt -noout -fingerprint
-echo -e "\nSHA1 Fingerprint from cert returned by AppGW"
-openssl x509 -in <(openssl s_client -connect 52.228.26.165:443 -prexit 2>/dev/null) -noout -fingerprint
-
