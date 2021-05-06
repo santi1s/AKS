04c4e6f5f6302ddbba6794a0b499edefdb8bb3ce
santi1s <52079665+santi1s@users.noreply.github.com>
2021-05-06 16:14:27 +0100

    Add files via upload

--- scripts/get-azure-vnet-eps.sh
+++ scripts/get-azure-vnet-eps.sh
@@ -25,7 +25,7 @@ printf "\t${b}%-15s %-15s %-20s %-40s %-40s${n}\n" "Endpoint" "Host IF" "IP" "Na
 
 for EP in $EPS;  do
         EPJ=$(cat /var/run/azure-vnet.json | jq ".Network.ExternalInterfaces.eth0.Networks.azure.Endpoints.\"${EP}\"")
-        IFNAME=$(echo $EPJ | jq -r '.IfName')
+        HIFNAME=$(echo $EPJ | jq -r '.HostIfName')
         IP=$(echo $EPJ | jq -r '.IPAddresses[].IP' )
         POD=$(echo $EPJ | jq -r '.PODName' )
         NS=$(echo $EPJ | jq -r '.PODNameSpace' )
