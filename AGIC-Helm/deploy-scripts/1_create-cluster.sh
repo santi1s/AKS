47bb456404b762bfa0ff9766a95213a4b80e77a9
snti1s <sergio.santiago@gmail.com>
2021-06-17 17:05:55 +0100

    update

--- AGIC-Helm/deploy-scripts/1_create-cluster.sh
+++ AGIC-Helm/deploy-scripts/1_create-cluster.sh
@@ -5,17 +5,17 @@ source 0_envvars.sh
 
 # Create Resource Group
 az group create -n $RG -l $LOC
-
+az network vnet subnet show --name aks-agicingress-nodesubnet --resource-group AGICIngress --vnet-name aks-agicingress-vnet -o tsv --query id
 
 # Create cluster
 echo -e "Create 1 Node CNI Cluster"
 az network vnet create -g  $RG --name $VNET_NAME --address-prefixes 10.3.0.0/16 -o none
 az network vnet subnet create -g $RG --vnet-name $VNET_NAME --name $NODE_SUBNET_NAME --address-prefixes 10.3.240.0/22 -o none
+SNET_ID=$(az network vnet subnet show --name $NODE_SUBNET_NAME --resource-group $RG --vnet-name $VNET_NAME -o tsv --query id)
 az aks create -n $CLUSTERNAME \
 -g $RG \
 --node-count 1 \
 --network-plugin azure \
 --generate-ssh-keys \
---vnet-subnet-id  /subscriptions/5a26ef37-cf5a-444a-b287-99a8aca1a85b/resourceGroups/$RG/providers/Microsoft.Network/virtualNetworks/$VNET_NAME/subnets/$NODE_SUBNET_NAME
-
+--vnet-subnet-id $SNET_ID
 
